7ea3c1fd8aa8f1dc7b20113525a660f6c4c4ca7b
Simon Renner <simon.renner.sport@gmail.com>
2018-11-29 07:12:59 +0100

    Turn pipeline into importable and executable script

--- spike_analysis/pipeline.py
+++ spike_analysis/pipeline.py
@@ -1,4 +1,16 @@
-# read the data
-# expect the data to be spikes as [1,0,0,1,1,1,0] and a samplingrate as 50
-# calculate the mean firing rate
-# save the data
+from os.path import dirname, abspath
+from spikelib import read_data, mean_rate, save_data
+from sys import argv
+
+filepath = dirname(abspath(__file__))[:-14] + 'data/'
+print(filepath)
+
+def main():
+    filename = 'exp1.txt'
+    spikes, samplingrate = read_data(filepath + filename)
+    mrate = mean_rate(spikes, samplingrate)
+    filename_analyzed = filename[:-4] + '_rate.txt'
+    save_data(filepath + filename_analyzed, mrate)
+    
+if __name__ == '__main__':
+    main()
