eebddb944abe854fc3933ec2ff03fcf4b9eaadbe
Simon Renner <simon.renner.sport@gmail.com>
2018-11-29 06:27:54 +0100

    Remove unnecessary patch for create_testfile function

--- test_pipeline.py
+++ test_pipeline.py
@@ -3,7 +3,6 @@ from mock import patch
 from os.path import isfile
 from os import remove
 
-@patch('spikelib.create_testfile')
 @patch('spikelib.read_data', return_value = ([1,0,1,1,1,0], 40))
 def test_mean_rate_pipeline_end_to_end(read_data, create_testfile):
     testfilename = 'testfile'
