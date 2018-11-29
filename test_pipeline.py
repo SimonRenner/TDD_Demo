b9dbde7dacbc36286b5d7fd252b40205cf3ad004
Simon Renner <simon.renner.sport@gmail.com>
2018-11-29 06:12:43 +0100

    Commment out remove and use os.path.isfile to make sure directory is clean of testfiles

--- test_pipeline.py
+++ test_pipeline.py
@@ -1,5 +1,6 @@
 from spikelib import mean_rate, read_data
 from mock import patch
+from os.path import isfile
 
 @patch('spikelib.create_testfile')
 @patch('spikelib.read_data', return_value = ([1,0,1,1,1,0], 40))
@@ -7,8 +8,8 @@ def test_mean_rate_pipeline_end_to_end(read_data, create_testfile):
     testfilename = 'testfile'
     create_testfile(testfilename)
     spikes, samplingrate = read_data(testfilename)
-    remove(testfilename)
-    assert exists(testfilename)
+    # remove(testfilename)
+    assert not isfile(testfilename)
     assert spikes == [1,0,1,1,1,0]
     assert samplingrate == 40
     mrate = mean_rate(spikes, samplingrate)
